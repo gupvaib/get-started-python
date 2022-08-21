234568ae38cf2b39da0250c0ba377644f69cb15b
Vaibhav Gupta <gupta.85@iitj.ac.in>
2022-08-21 10:18:58 +0530

    Info message added

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+print("Setup initialization with flask")
+
 setup(
     name='python-hello-world-flask',
     version='1.0.0',
